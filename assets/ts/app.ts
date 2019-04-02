5d45d93c48787ec95eead939891ca056d7bdb8cf
TheTakylo <sebastien.thuret@yahoo.fr>
2019-04-02 19:05:34 +0200

    Affichage & Gestion du formulaire de réservation

--- assets/ts/app.ts
+++ assets/ts/app.ts
@@ -5,7 +5,7 @@ require('../images/logo.png');
 
 import BikeApi from "./class/BikeApi/BikeApi";
 import LeafletMap from "./class/LeafletMap/LeafletMap";
-import Slider from "./class/Slider/Slider";
+import SimpleSign from "./class/SimpleSign/SimpleSign";
 
 const revervationMap = new LeafletMap({
     selector: 'map',
@@ -21,32 +21,65 @@ const bikeApi = new BikeApi({
     apiKey: '127afb8ee4f63cf897ba3944a3ffc79aa1616206'
 });
 
-let $defaultAlert = $('#reservation-card .alert.alert-secondary');
-let $form = $('#reservation-card form');
+const $defaultAlert = $('#reservation-card #default-alert');
+const $errorAlert = $('#reservation-card #error-alert');
+const $successAlert = $('#reservation-card #success-alert');
+const $form = $('#reservation-card form');
 
-let $stationDetail = $('#reservation-card #station-detail'),
-    $stationTitle = $stationDetail.find('.title'),
-    $stationAddress = $stationDetail.find('.address'),
-    $stationPlaces = $stationDetail.find('.places'),
-    $stationAvailableBikes = $stationDetail.find('.available_bikes');
+const $stationDetail = $('#reservation-card #station-detail'),
+$stationTitle = $stationDetail.find('.title'),
+$stationAddress = $stationDetail.find('.address'),
+$stationPlaces = $stationDetail.find('.places'),
+$stationAvailableBikes = $stationDetail.find('.available_bikes'),
+$stationState = $stationDetail.find('.state');
 
 bikeApi.load().then(data => {
     data.forEach(item => {
-
+        
         const position: [number, number] = [item.position.lat, item.position.lng];
         const icon = ( item.status === 'OPEN' ) ? 'marker-green.png' : 'marker-red.png';
-
+        
         const marker = revervationMap.addItem(position, "./build/images/" + icon);
         
+        // Gestion du click sur un marker (une station)
         marker.addEventListener('click', () => {
+            
             $defaultAlert.addClass('d-none');
             $form.removeClass('d-none');
             $stationDetail.removeClass('d-none');
-
+            
             $stationTitle.text(item.name);
             $stationAddress.text(item.address);
-            $stationPlaces.text(item.available_bike_stands);
-            $stationAvailableBikes.text(item.bike_stands);
+            $stationAvailableBikes.text(item.available_bike_stands);
+            
+            const textClassList = ['text-success', 'text-danger', 'text-warning'];
+            const badgesClassList = ['badge-success', 'badge-danger', 'badge-warning'];
+            
+            // Nombre de vélos 
+            // SI le nombre est supérieur à 5, le texte sera VERT
+            // SI le nombre est inférieur à 5 et supérieur à 2 , le texte sera ORANGE
+            // SI le nombre est inférieur à 2 , le texte sera ROUGE
+            if(item.bike_stands > 5) {
+                $stationPlaces.parent().removeClass(textClassList).addClass('text-success');
+                $stationPlaces.text(item.bike_stands);
+            } else if (item.bike_stands < 5) {
+                $stationPlaces.parent().removeClass(textClassList).addClass('text-danger');
+                $stationPlaces.text(item.bike_stands);
+            } else if (item.bike_stands < 2) {
+                $stationPlaces.parent().removeClass(textClassList).addClass('text-warning');
+                $stationPlaces.text(item.bike_stands);
+            }
+            
+            // Status de la stations
+            // VERT = OPEN
+            // ROUGE = CLOSE
+            if(item.status == 'OPEN') {
+                $stationState.text('Station ouverte');
+                $stationState.removeClass(badgesClassList).addClass('badge-success');
+            } else {
+                $stationState.text('Station fermée');
+                $stationState.removeClass(badgesClassList).addClass('badge-danger');
+            }
         })
     });
     
@@ -54,4 +87,58 @@ bikeApi.load().then(data => {
 });
 
 
-const slider = new Slider();
+const $reservationForm = $('#reservation-form');
+const $buttonStepOne = $reservationForm.find('button.substep1');
+const $buttonStepTwo = $reservationForm.find('button.substep2');
+
+// Click du boutton réserver
+$buttonStepOne.click(e => {
+    e.preventDefault();
+    
+    const firstname: string = $reservationForm.find("input[name='firstname']").val().toString().trim();
+    const lastname: string = $reservationForm.find("input[name='lastname']").val().toString().trim();
+    
+    // Vérification de la présence d'un nom & prénom
+    if(firstname == '' || lastname == '') {
+        // Message d'erreur
+        $errorAlert.removeClass('d-none');
+        return;
+    }
+    
+    $errorAlert.addClass('d-none');
+    
+    // On cache la premère partie & on affiche le canvas de signature
+    $reservationForm.find('.step-1').addClass('d-none');
+    $reservationForm.find('.step-2').removeClass('d-none');
+    
+    // Canvas de signature
+    const sign = SimpleSign.sign('canvasSignature');
+    
+    // TODO: Annulation de la réservation
+    $reservationForm.find('.form-cancel').click(e => { sign.reset(); });
+    // TODO: Remise à zéro du canvas 
+    $reservationForm.find('.sign-reset').click(e => { sign.reset(); });
+    
+    $buttonStepTwo.click(e => {
+
+        // Si le canvas (la signature) est vide
+        if(sign.isEmpty()) {
+            $errorAlert.removeClass('d-none');
+            return;
+        }
+
+        // On cache le canvas & les bouttons
+        $reservationForm.find('.step-2').addClass('d-none');
+
+        // Remise à zéro des champs
+        $reservationForm.find("input[name='firstname']").text("");
+        $reservationForm.find("input[name='lastname']").text("");
+        
+        
+        // On cache le message d'erreur
+        $errorAlert.addClass('d-none');
+        // Message de succès
+        $successAlert.removeClass('d-none');
+    });
+    
+});
---
+++ assets/ts/class/SimpleSign/SimpleSign.ts
@@ -0,0 +1,57 @@
+export default class SimpleSign {
+    
+    canvas;
+    context;
+    
+    height: number;
+    width: number;
+    draggable: boolean;
+    started: boolean = false;
+    
+    static sign(element: string) {
+        return new SimpleSign(element);
+    };
+    
+    constructor(element: string) {
+        this.canvas = document.getElementById(element);
+        this.context = this.canvas.getContext("2d");
+        
+        this.height = this.canvas.height;
+        this.width = this.canvas.width;
+        
+        this.context.clearRect(0, 0, this.height, this.width);
+        this.context.lineWidth = 2;
+        this.context.strokeStyle = '#000000';
+        
+        this.canvas.addEventListener('mousedown', this.start.bind(this));
+        this.canvas.addEventListener('mousemove', this.draw.bind(this));
+        this.canvas.addEventListener('mouseup', this.end.bind(this));
+    };
+    
+    public isEmpty(): boolean {
+        return !this.started;
+    }
+    
+    public reset(): void {
+        this.context.clearRect(0, 0, this.height, this.width);
+    }
+    
+    private start(event): void {
+        this.started = true;
+        this.draggable = true;
+        
+        this.context.moveTo(event.offsetX, event.offsetY);
+    };
+    
+    private end(): void {
+        this.draggable = false;
+    };
+    
+    private draw(event): void {
+        if(this.draggable) {
+            this.context.lineTo(event.offsetX, event.offsetY);
+            this.context.stroke();        
+        }
+    };
+    
+};
